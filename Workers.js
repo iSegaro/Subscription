d2b0da3fe616e5c1fdc9152804501db7d713bfa7
iSegaro <121983867+iSegaro@users.noreply.github.com>
2023-12-01 05:03:03 +0330

    Create Workers.js

---
+++ Workers.js
@@ -0,0 +1,31 @@
+async function handleRequest(request) {
+  // How to create a subscribe link and give it to the family with iSegaro
+    const url = new URL(request.url);
+    const provider = url.searchParams.get("subs");
+  
+    let links = [];
+    if (provider === "IRC") {
+      links = [
+        'xxxxxx',
+        'xxxxxx',
+        'xxxxxx'
+      ];
+    } else if (provider === "MCI") {
+        links = [
+          'xxxxxx'
+        ];
+      }
+  
+    const responseText = links.join('\n').replace(/,/g, '\n');
+  
+    // Encoding the response text in base64
+    const encodedResponse = btoa(responseText);
+
+    return new Response(encodedResponse, {
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
+  
+  addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request));
+  });
